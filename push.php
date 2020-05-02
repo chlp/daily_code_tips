6fbe20a4213beebf629a2a57a8121da09a954502
Alexey R <user.raa@gmail.com>
2020-05-03 01:53:17 +0300

    Create push.php

---
+++ push.php
@@ -0,0 +1,22 @@
+<?php
+$secret = json_decode(file_get_contents(__DIR__ . '/../secret.json'), true);
+$slackService = $secret['slack_service'] ?? null;
+if ($slackService === null) {
+    return;
+}
+$tips = json_decode(file_get_contents(__DIR__ . '/tips.json'), true);
+$tip = $tips[array_rand($tips)];
+$text = "*{$tip['title']}*\r\n";
+foreach ($tip['points'] as $point) {
+    $text .= "* {$point}\r\n";
+}
+$text .= "_{$tip['link']}_";
+$url = 'https://hooks.slack.com/services/' . $slackService;
+$result = file_get_contents($url, false, stream_context_create([
+    'http' => [
+        'method' => 'POST',
+        'header' => 'Content-type: application/json',
+        'content' => json_encode(['text' => $text])
+    ]
+]));
+// $result === 'ok'
