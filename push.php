3f1c4d73219f1a2238241c6740d75f539b43278b
a.rytikov <a.rytikov@city-mobil.ru>
2020-06-13 15:23:49 +0300

    fix push.php: лишнее копипастнул

--- push.php
+++ push.php
@@ -7,13 +7,6 @@ if (PHP_SAPI !== 'cli') {
 require_once __DIR__ . '/DailyTip.php';
 require_once __DIR__ . '/Slack.php';
 
-$input = @json_decode(file_get_contents('php://input'), true);
-if (!is_array($input)) {
-    return;
-}
-
-file_put_contents(__DIR__ . '/events/' . time() . '.txt', json_encode([$input]));
-
 $todayTip = (new DailyTip())->getRandomText();
 $slack = new Slack();
 foreach ($slack->getAppConversations() as $conversationId) {
